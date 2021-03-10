f3339d0dad2e63dff903a1ba3f5ae0b6cf395403
Nisha <nk1655550@gmail.com>
2021-03-10 10:45:00 -0500

    change multiple images

---
+++ JAVASCRIPT/REGULAREXP/script.js
@@ -0,0 +1,9 @@
+function changeImage(){
+    //set Value
+    document.getElementById("output").src="c2.jpg";
+}
+function changeImageOut(){
+    
+    //set Value
+    document.getElementById("output").src="c1.png";
+}
---
+++ JAVASCRIPT/TASK/imagechanging.js
@@ -0,0 +1,38 @@
+
+function changeImage(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImageOver(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImage1(){
+    document.getElementById("output").src="flower.jpg";
+}
+function changeImageOut1(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImage2(){
+    document.getElementById("output").src="nature3.jpg";
+}
+function changeImageOut2(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImage3(){
+    document.getElementById("output").src="nature4.jpg";
+}
+function changeImageOut3(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImage4(){
+    document.getElementById("output").src="nature5.jpg";
+}
+function changeImageOut4(){
+    document.getElementById("output").src="nature1.jpg";
+}
+function changeImage5(){
+    document.getElementById("output").src="nature6.jpg";
+}
+function changeImageOut5(){
+     document.getElementById("output").src="nature1.jpg";
+}
+

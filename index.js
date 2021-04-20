9782ac403b55136250ad5e4bd8ec39176ab5753f
nisha-902 <nk1655550@gmail.com>
2021-04-19 19:51:49 -1200

    installation of nodemon and express server

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
 const fs=require("fs")
 const http=require("http")
 const url=require('url')
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
 // const readfile=fs.readFileSync('meanstack.txt', 'utf-8')
 // console.log(readfile)
 
-// const newtext="hello class, this is new file and created at "+Date.now();
-// fs.writeFileSync('newfile.txt',newtext)
-// console.log('writing complete')
- const server= http.createServer((req, res)=>{
-    console.log(req.url)
-//     const query=url.parse(req.url,true)
-//    console.log(query)
+// // const newtext="hello class, this is new file and created at "+Date.now();
+// // fs.writeFileSync('newfile.txt',newtext)
+// // console.log('writing complete')
+//  const server= http.createServer((req, res)=>{
+//     console.log(req.url)
+// //     const query=url.parse(req.url,true)
+// //    console.log(query)
     
-    const {query, pathname} =  url.parse(req.url,true)
-     console.log(req.url) 
-     const path=req.url
-     if(pathname===('/'))
-     {
-        console.log(query)
-    res.end("result available");
-     }
-     else if(pathname==='/addcourse'){
-         res.end('add course works')
-     }
-     else if(pathname==='/student')
-     {
-         fs.readFile("./data/data.txt", "utf-8", (err, data)=>{
-            //const mydata=JSON.parse(data)
-             console.log("API work")
-             res.writeHead(200, {'context-type':'text/javascript'})
-             res.end(data)
+//     const {query, pathname} =  url.parse(req.url,true)
+//      console.log(req.url)
+//      const path=req.url
+//      if(pathname===('/'))
+//      {
+//         console.log(query)
+//     res.end("result available");
+//      }
+//      else if(pathname==='/addcourse'){
+//          res.end('add course works')
+//      }
+//      else if(pathname==='/student')
+//      {
+//          fs.readFile("./data/data.txt", "utf-8", (err, data)=>{
+//             //const mydata=JSON.parse(data)
+//              console.log("API work")
+//              res.writeHead(200, {'context-type':'text/javascript'})
+//              res.end(data)
 
-         })
+//          })
          
-         fs.readFile("./data/result.json", "utf-8", (err, data)=>{
+//          fs.readFile("./data/result.json", "utf-8", (err, data)=>{
             
-             console.log(data)
-            // res.end("API works"))
-         })
+//              console.log(data)
+//             // res.end("API works"))
+//          })
 
 
-     }
-     else{
-         res.writeHead(404,{
-             'content-type':'text/html',
-             'my-own-header':'hello world'
-         }) 
-         res.end('404 page not found')
-     }
+//      }
+//      else{
+//          res.writeHead(404,{
+//              'content-type':'text/html',
+//              'my-own-header':'hello world'
+//          }) 
+//          res.end('404 page not found')
+//      }
 
 
-});
-server.listen(8080, '127.0.0.1',()=>{
-    console.log('server run at 127.0.0.1:8080');
-});
+// });
+// server.listen(8080, '127.0.0.1',()=>{
+//     console.log('server run at 127.0.0.1:8080');
+// });
